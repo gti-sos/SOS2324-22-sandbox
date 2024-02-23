19324683ff5a2af24f4e332daf50b5242e8890e4
Daniel Taillefer <danieltaillefernieto@gmail.com>
2024-02-23 12:31:20 +0100

    ultima cosa de la clase

---
+++ api-contacts/index.js
@@ -0,0 +1,26 @@
+const API_BASE = "/api/v1";
+
+var contacts = [
+    {
+        name: "pepe",
+        phone: 12456789
+    },
+    {
+        name: "juan",
+        phone: 1243234789
+    }
+];
+
+
+
+module.exports = (app) => {
+    app.get(API_BASE+"/contacts", (req,res)=> {
+        res.send(JSON.stringify(contacts));
+    })
+    
+    app.post(API_BASE+"/contacts", (req,res)=>{
+        let contact = req.body;
+        contacts.push(contact);
+        res.sendStatus(201,"Created");
+    })
+};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let cool = require("cool-ascii-faces");
 let bodyParser = require("body-parser");
-
 let express = require("express");
+let contactAPI = require("./api-contacts");
 
 let app = express();
 
@@ -9,35 +9,14 @@ app.use(bodyParser.json());
 
 app.use("/",express.static("./public"));
 
+contactAPI(app);
+
 const PORT = (process.env.PORT || 10000);
 
 
 app.get("/cool", (req,res)=>{
     res.send(`<html><body><h1>${cool()}</h1><body><html>`);
 });
-
-const API_BASE = "/api/v1";
-
-var contacts = [
-    {
-        name: "pepe",
-        phone: 12456789
-    },
-    {
-        name: "juan",
-        phone: 1243234789
-    }
-];
-
-app.get(API_BASE+"/contacts", (req,res)=> {
-    res.send(JSON.stringify(contacts));
-})
-
-app.post(API_BASE+"/contacts", (req,res)=>{
-    let contact = req.body;
-    contacts.push(contact);
-})
-
 
 
 app.listen(PORT, ()=> {
